dfc73a67c1a6d1bc8f50c56c48aca275f243fb18
josh-huang <joshhuang1999@gmail.com>
2022-06-17 15:59:38 +0800

    Working on MOH update function[C

--- SafeEntry/SafeEntry_client.py
+++ SafeEntry/SafeEntry_client.py
@@ -29,59 +29,58 @@ def run():
         # indicate the stub 
         stub = SafeEntry_pb2_grpc.SafeEntryStub(channel)
         
+        while True: 
         # check in and check out for individual and group 
-        user_choice = str(input("\nWhich function do you wish to perform?\n [1]. Check in\n [2]. Check out\n [3]. Group Check in\n [4]. Group Check out\n [4]. Display all the locations\n"))
-        
-        # user choose Check in function 
-        if user_choice == "1":
-            # user check in location (use random location to simulate)
-            user_location = random.choice(random_location)
-            # append the location in all_location and current_location array 
-            all_location.append(user_location)
-            current_location.append(user_location)
-            # get current time 
-            current_time = getCurrentTime()
-            response = stub.checkIn(SafeEntry_pb2.CheckInRequest(name=user_name, id=user_id, location=user_location, check_in_time=current_time))
-            print(str(response.res))
-        
-        # user choose Check out function 
-        elif user_choice == "2":
-            # user check out location in the current location array 
-            check_out_location = random.choice(current_location)
-            current_time = getCurrentTime()
-            current_location.remove(check_out_location)
-            response = stub.checkIn(SafeEntry_pb2.CheckInRequest(name=user_name, id=user_id, location=check_out_location, check_in_time=current_time))
-            print(str(response.res))
-        
-        # user choose Group check in function 
-        elif user_choice == "3":
-            # group check in function 
-            pass
-        
-        # user choose Group check out function 
-        elif user_choice == "4":
-            # group check out function 
-            pass
-        
-        # user choose display location history function 
-        elif user_choice == "5":
-            # display location function  
-            pass
-        
-        # receive sms notification if visited covid places 
+            user_choice = str(input("\nWhich function do you wish to perform?\n [1]. Check in\n [2]. Check out\n [3]. Group Check in\n [4]. Group Check out\n [5]. Display all the locations\n"))
+            
+            # user choose Check in function 
+            if user_choice == "1":
+                # user check in location (use random location to simulate)
+                user_location = random.choice(random_location)
+                # append the location in all_location and current_location array 
+                all_location.append(user_location)
+                current_location.append(user_location)
+                # get current time 
+                current_time = getCurrentTime()
+                response = stub.checkIn(SafeEntry_pb2.CheckInRequest(name=user_name, id=user_id, location=user_location, check_in_time=current_time))
+                print(str(response.res))
+            
+            # user choose Check out function 
+            elif user_choice == "2":
+                # user check out location in the current location array 
+                check_out_location = random.choice(current_location)
+                current_time = getCurrentTime()
+                current_location.remove(check_out_location)
+                response = stub.checkIn(SafeEntry_pb2.CheckInRequest(name=user_name, id=user_id, location=check_out_location, check_in_time=current_time))
+                print(str(response.res))
+            
+            # user choose Group check in function 
+            elif user_choice == "3":
+                # group check in function 
+                pass
+            
+            # user choose Group check out function 
+            elif user_choice == "4":
+                # group check out function 
+                pass
+            
+            # user choose display location history function 
+            elif user_choice == "5":
+                # display location function  
+                pass
+            
+            # receive sms notification if visited covid places 
         
 
         response = stub.Multiply(SafeEntry_pb2.Request(x=5, y=6))
         print("The result of Add Function is: " + str(response.res))
 
-        response = stub.Divide(SafeEntry_pb2.Request(x=5, y=6))
-        print("The result of Add Function is: " + str(response.res))
         
 # function to return all the location that visited by the client 
-def getLocation():
+def getAllLocation():
     # get location from the list 
     for i in all_location:
-        print(str(i))
+        print(str(i)+'\n')
 
 def getCurrentTime():
     now = datetime.now()
--- SafeEntry/SafeEntry_server.py
+++ SafeEntry/SafeEntry_server.py
@@ -29,8 +29,10 @@ class SafeEntry(SafeEntry_pb2_grpc.SafeEntryServicer):
     def checkIn(self, request, context):
         if request.name not in client_info:
             client_info[request.name]=[request.id, request.location, request.check_in_time]
+        
+        reply_message = 'Check In ' + request.location + ' successful' +
             
-        return SafeEntry_pb2.CheckInReply(res='Check In ' + request.location + ' successful')
+        return SafeEntry_pb2.CheckInReply(res=reply_message)
     
     # individual check out function 
     def checkOut(self, request, context):
@@ -53,13 +55,15 @@ class SafeEntry(SafeEntry_pb2_grpc.SafeEntryServicer):
             
         return SafeEntry_pb2.Reply(res='Group Check Out ' + request.location + ' successful')
     
-    # for MOH use 
+    # MOH update location 
     def updateLocation(self, request, context):
-        #TO-DO Implement update location 
+        
+        return SafeEntry_pb2.MOHReply(res='Updates have been received.')
+    
+    # send sms notification to the clients who has visited covid places
+    def getNotification(self, request, context):
+        
         pass
-        # receive location name and iterate through location info and send notification to all the names in the array 
-
-
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
